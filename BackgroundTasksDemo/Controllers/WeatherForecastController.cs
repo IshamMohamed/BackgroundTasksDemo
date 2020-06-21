6131f6ef4d31f26c8e7259a30594aef4ced33f1a
Isham Mohamed Muhammetu Iqbal <Isham@kloudynet.com>
2020-06-21 23:37:52 +0800

    Remove and sort usings

--- BackgroundTasksDemo/Controllers/WeatherForecastController.cs
+++ BackgroundTasksDemo/Controllers/WeatherForecastController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BackgroundTasksDemo.BackgroundWorker;
+﻿using BackgroundTasksDemo.BackgroundWorker;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BackgroundTasksDemo.Controllers
 {
