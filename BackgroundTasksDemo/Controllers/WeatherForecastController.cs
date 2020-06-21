3336f1d9629675b517fed3ad715d4bfc06a85250
Isham Mohamed Muhammetu Iqbal <Isham@kloudynet.com>
2020-06-22 00:03:46 +0800

    Added comments etc

--- BackgroundTasksDemo/Controllers/WeatherForecastController.cs
+++ BackgroundTasksDemo/Controllers/WeatherForecastController.cs
@@ -8,17 +8,15 @@ using System.Linq;
 namespace BackgroundTasksDemo.Controllers
 {
     [ApiController]
-    // Below is commented because of: https://twitter.com/Isham_M_Iqbal/status/1274724738682613761
-    //[Route("[controller]/[action]")]
+    // Below is modified because of: https://twitter.com/Isham_M_Iqbal/status/1274724738682613761
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private IBackgroundTaskQueue queue;
-
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+        private IBackgroundTaskQueue queue;
         private readonly ILogger<WeatherForecastController> logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger, IBackgroundTaskQueue queue)
@@ -27,6 +25,10 @@ namespace BackgroundTasksDemo.Controllers
             this.queue = queue;
         }
 
+        /// <summary>
+        /// This is a long running job
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("get")]
         public IEnumerable<WeatherForecast> Get()
@@ -42,16 +44,33 @@ namespace BackgroundTasksDemo.Controllers
             .ToArray();
         }
 
+        /// <summary>
+        /// A method to queue a long running job and quickly return Accepted status
+        /// If a Guid provided, this will check and return the result of that 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet]
-        [Route("getasync")]
-        public IActionResult GetAsync()
-        {
-            queue.QueueBackgroundWorkItem(async token =>
+        [Route("getasync/{id?}")]
+        public IActionResult GetAsync(Guid? id)
+        { 
+            if (id != null)
             {
-                Get();
-            });
+                // execute the logic to get the status of a previous Get task
+                return Ok();
+            }
+            else
+            {
+                Guid taskId = Guid.NewGuid();
 
-            return Accepted();
+                // queue the long running job and return Accepted
+                queue.QueueBackgroundWorkItem(async token =>
+                {
+                    Get();
+                });
+
+                return Accepted();
+            }
         }
     }
 }
